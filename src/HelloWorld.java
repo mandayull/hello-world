155d89eea4cb308e556302c6f21534b505d89e31
Administrator <Administrator@MSDN-SPECIAL>
2018-02-21 15:40:02 +0900

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,21 @@
+
+public class HelloWorld 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+		System.out.println("I'm Manda");
+		System.out.println(12);
+		System.out.println("12");
+		System.out.println(6+6);
+		System.out.println("1"+"2");
+		System.out.println(1+"2");
+		System.out.println("1"+2);
+		System.out.println(13-1);
+		System.out.println((2+4)+(2+4));
+		System.out.println((12));
+		System.out.println("2+5="+2+5);
+		System.out.println("2+5="+(2+5));
+		System.out.println((2+5)+(2+5));
+	}
+}
