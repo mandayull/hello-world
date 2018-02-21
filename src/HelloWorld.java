06d9d8d67b7cbc15be4f86213bc9ff467fe16ad4
Administrator <Administrator@MSDN-SPECIAL>
2018-02-21 15:47:40 +0900

    Yull 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld
 	{
 		System.out.println("Hello World");
 		System.out.println("I'm Manda");
+		System.out.println("Yull");
 		System.out.println(12);
 		System.out.println("12");
 		System.out.println("1"+"2");
